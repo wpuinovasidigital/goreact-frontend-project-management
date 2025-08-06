c31a2c0f0e169ae54dbd2c6646e3d54b13c5f411
Muhammad Agung Rizkyana <muhammadagungrizkyana@Muhammads-MacBook-Pro.local>
2025-08-06 14:19:29 +0700

    feat: membuat komponen Forms/TextField

--- src/components/pages/Auth/Login/Login.jsx
+++ src/components/pages/Auth/Login/Login.jsx
@@ -2,9 +2,16 @@ import { Paper, Stack } from '@mui/material';
 import { useForm } from 'react-hook-form';
 
 import Select from '../../../ui/Forms/Select';
+import TextField from '../../../ui/Forms/TextField';
 
 const Login = () => {
-  const { control } = useForm();
+  const { control, watch } = useForm();
+
+  const username = watch('username')
+  const category = watch('category')
+
+  console.log("username:", username)
+  console.log("category:", category);
 
   return (
     <Stack
@@ -19,8 +26,9 @@ const Login = () => {
           padding: 2,
         }}
       >
+        <TextField name={"username"} control={control} label={"Username"} />
         <Select
-          name={'kategori'}
+          name={'category'}
           control={control}
           label={'Pilih Kategori'}
           options={[
