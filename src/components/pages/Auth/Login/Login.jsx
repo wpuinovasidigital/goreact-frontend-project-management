fcfc181009566e7b2486d0165bb0092de2ba4710
Muhammad Agung Rizkyana <muhammadagungrizkyana@Muhammads-MacBook-Pro.local>
2025-08-08 09:34:38 +0700

    feat: materi path alias

--- src/components/pages/Auth/Login/Login.jsx
+++ src/components/pages/Auth/Login/Login.jsx
@@ -2,9 +2,9 @@ import { Paper, Stack } from '@mui/material';
 import dayjs from 'dayjs';
 import { useForm } from 'react-hook-form';
 
-import DatePicker from '../../../ui/Forms/DatePicker';
-import Select from '../../../ui/Forms/Select';
-import TextField from '../../../ui/Forms/TextField';
+import DatePicker from '@/components/ui/Forms/DatePicker';
+import Select from '@/components/ui/Forms/Select';
+import TextField from '@/components/ui/Forms/TextField';
 
 const Login = () => {
   const { control, watch } = useForm({
